babc62963863038f1dd862854ab9c882c164592e
Syrious <syrious@gmx.net>
2024-04-10 11:38:18 +0200

    Uses sheet-only-plus API (#24)

--- scripts/api.js
+++ scripts/api.js
@@ -2,25 +2,19 @@ import {isSheetOnly, currentActor} from "./main.js";
 /* global game, Hooks*/
 
 
-Hooks.on('setup', async () => {
+Hooks.once('setup', async () => {
     setupApi();
 });
-function setupApi() {
 
+function setupApi() {
     game.modules.get('sheet-only').api = {
         // Compatibility with plus version
-        plusCompatibility: "0.1.0",
-
-        isSheetOnly: function () {
-            return isSheetOnly();
-        },
+        plusCompatibility: "0.2.0",
 
         getActor: function () {
             return currentActor;
         }
 
-        // exampleVariable: "Custom Module Variable",
-        //
         // ExampleClass: class {
         //     constructor(name) {
         //         this.name = name;
