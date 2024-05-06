2bb0d5ce324afd815be685af7503062e3941a062
dbrinkmann <dbrinkmann@neusta.de>
2024-05-06 09:01:50 +0200

    Requires plus 0.1.3

--- scripts/api.js
+++ scripts/api.js
@@ -9,7 +9,7 @@ Hooks.once('setup', async () => {
 function setupApi() {
     game.modules.get('sheet-only').api = {
         // Compatibility with plus version
-        plusCompatibility: "0.1.2",
+        plusCompatibility: "0.1.3",
 
         getActor: function () {
             return currentActor;
