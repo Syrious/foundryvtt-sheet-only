cc3bf1f8a06063a9bba3da3372b60d55f79de89d
dbrinkmann <dbrinkmann@neusta.de>
2024-05-26 12:59:30 +0200

    Enable movement controls

--- scripts/addControlButtons.js
+++ scripts/addControlButtons.js
@@ -76,13 +76,12 @@ export function addControlButtons(sheetContainer, increaseZoom, decreaseZoom, re
 
         controllingButton.on("click", function () {
             if (wasDragged()) return;
-            wipDialog("Movement", "It will let you move your tokens.");
 
-            // if (window.sheetOnlyPlus && typeof window.sheetOnlyPlus.openControls === "function") {
-            //     window.sheetOnlyPlus.openControls();
-            // } else {
-            //    showPatreonDialog("Movement");
-            // }
+            if (window.sheetOnlyPlus && typeof window.sheetOnlyPlus.openControls === "function") {
+                window.sheetOnlyPlus.openControls();
+            } else {
+                showPatreonDialog("Movement");
+            }
         });
 
         menuButton.on("click", function () {
