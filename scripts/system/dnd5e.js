7a8aa1b0ebca6269f082ada06c4e793a29cf7cd1
Eddie Cooro <eddiecooro@gmail.com>
2024-08-18 00:02:04 +0330

    Listen for messages from the chat and open if item use by this user

--- scripts/system/dnd5e.js
+++ scripts/system/dnd5e.js
@@ -1,13 +1,37 @@
+import {openChat} from "../chat.js";
+
 export function dnd5eReadyHook() {
     if (!isDnd5e()) {
         return
     }
 
     document.body.addEventListener("click", onActivate, true);
+
+    Hooks.on("renderChatMessage", handleChatMessage)
 }
 
 export function isDnd5e() {
     return game.system.id === 'dnd5e';
+}
+
+/**
+ * Handle use items
+ */
+function handleChatMessage(message, _html, messageData){
+    // Check if this user is the author of the message
+    if (!messageData.author.isSelf) return;
+
+    // Check if the messsage is usage of an item
+    const useFlag = message.getFlag("dnd5e", "use")
+    if (!useFlag) return;
+
+    /**
+     * Open chat and scroll to bottom.
+     * It might be possible to scroll to the specific message. But from my experience, it doesn't worth the effort.
+     * Since this message is highly likely to be the last message (Given that we're scrolling right after it is sent)
+     */
+    openChat()
+    game.messages.directory.scrollBottom();
 }
 
 /**
