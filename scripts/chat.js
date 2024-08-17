686578b5080b9d2ea7e5b358b31765fc4aa52a42
Eddie Cooro <eddiecooro@gmail.com>
2024-08-17 23:56:33 +0330

    Move setupChatPanel to chat.js

--- scripts/chat.js
+++ scripts/chat.js
@@ -23,3 +23,24 @@ export function toggleChat() {
     const isCollapsed = $sheet.hasClass('collapse');
     updateChatState(!isCollapsed);
 }
+
+export function setupChatPanel() {
+    const chatElement = $('#chat'); // Get the chat element
+    const newParentElement = $('.sheet-only-container'); // Get the new parent
+
+    if (chatElement.length && newParentElement.length) {
+        // Create a new div and wrap the chat element inside it
+        chatElement.wrap('<div id="chat-wrapper"></div>');
+
+        // Get the wrapper we just created along with its child
+        const chatElementWrapper = $('#chat-wrapper');
+        chatElementWrapper.addClass("sheet-only-chat");
+        chatElementWrapper.addClass('collapse');
+        chatElementWrapper.addClass('so-draggable');
+
+        chatElementWrapper.detach(); // Remove the wrapped chatElement (along with its wrapper) from the DOM
+        newParentElement.append(chatElementWrapper); // Append the wrapped chatElement (with its wrapper) to the new parent
+    } else {
+        console.log("Could not find chat panel")
+    }
+}
