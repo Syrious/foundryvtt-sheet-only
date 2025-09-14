93e0e167bf0116a6c3b74c38f459bd2ceada2b41
Syrious <syrious@gmx.net>
2025-09-14 07:55:28 +0200

    Bugfix/issues with journal popout (#78)

--- src/journal.js
+++ src/journal.js
@@ -1,13 +1,27 @@
 import {renderOnSidebar} from "./sidebar";
+import {isSheetOnly} from "./utils";
 
 let journalPopout;
+
+export function onRenderJournalDirectory(app, html) {
+    if (isSheetOnly()) {
+
+        app.setPosition({
+            left: window.innerWidth,
+            top: 0,
+            zIndex: 9999
+        });
+
+        app.classList.add('so-draggable');
+    }
+}
 
 export function openJournal() {
     game.journal.apps[0]?.renderPopout().then(journalApp => {
         journalPopout = journalApp;
         journalPopout.classList.add("so-draggable");
 
-        if(window.innerWidth > 800) {
+        if (window.innerWidth > 800) {
             renderOnSidebar(journalPopout);
         }
 
