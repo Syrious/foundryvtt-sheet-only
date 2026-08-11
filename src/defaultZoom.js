bb03e4b60549f25bbe5544b667c330ea3f49c91c
Syrious <syrious@gmx.net>
2026-08-11 11:15:09 +0200

    Improves zoom functionality

--- src/defaultZoom.js
+++ src/defaultZoom.js
@@ -1,23 +1,23 @@
 export function increaseZoom() {
     let sheet = $('.sheet-only-sheet');
-    let scaleFactor = parseFloat(sheet.css('zoom'));
+    let scaleFactor = parseFloat(sheet.css('--zoom-level')) || 1;
 
     scaleFactor += 0.1;
 
-    sheet.css({ zoom: scaleFactor });
+    sheet.css({ '--zoom-level': scaleFactor });
 }
 
 export function decreaseZoom() {
     let sheet = $('.sheet-only-sheet');
-    let scaleFactor = parseFloat(sheet.css('zoom'));
+    let scaleFactor = parseFloat(sheet.css('--zoom-level')) || 1;
 
     scaleFactor = Math.max(scaleFactor - 0.1, 0.1);
 
-    sheet.css({ zoom: scaleFactor });
+    sheet.css({ '--zoom-level': scaleFactor });
 }
 
 export function resetZoom() {
     let sheet = $('.sheet-only-sheet');
 
-    sheet.css({ zoom: 1 });
+    sheet.css({ '--zoom-level': 1 });
 }
